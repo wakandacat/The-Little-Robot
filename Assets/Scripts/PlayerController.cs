f40cad732082c398d23d92d2cc4b3c383c949565
linajaouani <linajaouani2019@gmail.com>
2025-01-03 17:24:04 -0500

    stash all

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -57,9 +57,6 @@ public class PlayerController : MonoBehaviour
     public bool isPaused = false;
     public GameObject pauseMenu;
 
-  
-
-
     void Start()
     {
         pc = new PlayerControls();     
