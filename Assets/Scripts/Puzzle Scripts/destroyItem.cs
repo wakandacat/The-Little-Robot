014d9659c4b259e4f007cf249612db618e24b8d7
ginetteGonzales <ginette.gonzales67@gmail.com>
2025-02-13 23:03:11 -0500

    new tag for crates, prefabs updated, box colliders on p1 (needs mod for second half), ground check updated to use crates and floor, destoryitem modified for player and crates, jump values tweaked

--- Assets/Scripts/Puzzle Scripts/destroyItem.cs
+++ Assets/Scripts/Puzzle Scripts/destroyItem.cs
@@ -8,7 +8,7 @@ public class destroyItem : MonoBehaviour
     private void OnTriggerEnter(Collider collider)
     {
        // Debug.Log("destroyed");
-        if(collider.gameObject.tag != "Player")
+        if(collider.gameObject.tag != "Player" && collider.gameObject.tag != "ground")
         {
             Destroy(collider.gameObject);
 
