15352ff744dfad0f59e2e4079856a335592c2187
ginetteGonzales <94503619+ginetteGonzales@users.noreply.github.com>
2025-02-18 22:24:47 -0500

    player sounds, narrative room + p2 updated (#41)
    * p2 narrative room added, needs testing
    
    * death pits in p2, animator on player
    
    * new player sounds added, new narrative room added
    
    * p2 vent instances updated to fit model, needs to be checked against player size, new player sounds
    
    * colliders on narrative room
    
    * temp light added to narrative room
    
    * death pit fungus prefab temp
    
    * controls png updated

--- Assets/Scripts/Player/player_fx_behaviors.cs
+++ Assets/Scripts/Player/player_fx_behaviors.cs
@@ -39,7 +39,15 @@ public class player_fx_behaviors : MonoBehaviour
         animationCalls();
     }
 
-    
+    void FixedUpdate()
+    {
+        animationCalls();
+    }
+
+    void LateUpdate()
+    {
+        animationCalls();
+    }
 
     public void animationCalls()
     {
@@ -52,34 +60,83 @@ public class player_fx_behaviors : MonoBehaviour
 
             //if the player is moving then trigger the walk animation
             //update this code to keep player in ball if rolling
+            //if (playerScript.leftStick.magnitude > 0.1f)
+            //{
+            //    /* if (playerScript.rollCounter == 1)
+            //     {
+            //         m_animator.SetBool("isRolling", true);
+            //     }
+            //     else
+            //     {
+            //     }*/
+            //    m_animator.SetBool("isWalking", true);
+
+            //}
+            //else
+            //{
+            //    //end walking or rolling animations 
+            //    m_animator.SetBool("isRolling", false);
+            //    m_animator.SetBool("isWalking", false);
+            //    //m_animator.SetFloat("walkSpeed", 1.25f);
+            //}
+
+            //if (playerScript.rollCounter == 1)
+            //{
+            //    m_animator.SetBool("isRolling", true);
+            //}
+            //else if (playerScript.rollCounter == 0)
+            //{
+            //    m_animator.SetBool("isRolling", false);
+
+            //}
+
+            //if player is moving
             if (playerScript.leftStick.magnitude > 0.1f)
             {
-                /* if (playerScript.rollCounter == 1)
-                 {
-                     m_animator.SetBool("isRolling", true);
-                 }
-                 else
-                 {
-                 }*/
-                m_animator.SetBool("isWalking", true);
+
+                //check if in roll
+                if (playerScript.rollCounter == 1)
+                {
+                    //roll
+                    m_animator.SetTrigger("roll");
+                    m_animator.SetBool("isRolling", true);
+                }
+                else if(playerScript.rollCounter == 0)
+                {
+                    m_animator.SetBool("isRolling", false);
+                    m_animator.SetTrigger("walk");
+                }
+                else
+                {
+                    //walking
+                    m_animator.SetTrigger("walk");
+                }
+
+            }
+            else if(playerScript.rollCounter == 1)//no longer moving
+            {
+                m_animator.SetTrigger("roll");
+                m_animator.SetBool("isRolling", true);
+
+                ////check if in roll -> yes just pause
+                //if(playerScript.rollCounter == 1)
+                //{
+                //    //roll
+                //}
+                //else //otherwise stop all movement
+                //{
+                //    //go to idle
+                //    m_animator.SetTrigger("idle");
+                //}
 
             }
             else
             {
-                //end walking or rolling animations 
+                m_animator.SetFloat("walkSpeed", 1);
+                m_animator.SetFloat("rollSpeed", 1);
                 m_animator.SetBool("isRolling", false);
-                m_animator.SetBool("isWalking", false);
-                m_animator.SetFloat("walkSpeed", 1.25f);
-            }
-         
-            if (playerScript.rollCounter == 1)
-            {
-                m_animator.SetBool("isRolling", true);
-            }
-            else if (playerScript.rollCounter == 0)
-            {
-                m_animator.SetBool("isRolling", false);
-
+                m_animator.SetTrigger("idle");
+                
             }
         }
 
