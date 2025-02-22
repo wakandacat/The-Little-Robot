a9364956f70d1695c2377a6f000b18f6145d2497
linajaouani <linajaouani2019@gmail.com>
2025-02-22 15:54:06 -0500

    Enemy Health Bar added

--- Assets/Scripts/Boss Enemy/EnemyUI.cs
+++ Assets/Scripts/Boss Enemy/EnemyUI.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class EnemyUI : MonoBehaviour
 {
     public GameObject enemyUI;
     public Image enemyHealthBar;
     public GameObject enemy;
-    private PlayerController playerScript;
-    UnityEngine.SceneManagement.Scene currentScene;
     // Start is called before the first frame update
     void Start()
     {
-        currentScene = SceneManager.GetActiveScene();
-        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        decrementBar();
+    }
+    public void decrementBar()
+    {
+        var maxenemyHealth = enemy.GetComponent<BossEnemy>().HP_Maximum;
+        var enemyCurrentHealth = enemy.GetComponent<BossEnemy>().HP_ReturnCurrent();
+
+        enemyHealthBar.fillAmount = (enemyCurrentHealth / maxenemyHealth);
     }
 }
