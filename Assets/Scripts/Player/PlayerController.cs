05a3b06673e16498b9f1f1707070989bf4bfc59d
linajaouani <linajaouani2019@gmail.com>
2025-04-03 15:54:17 -0400

    platform haptics

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -48,7 +48,7 @@ public class PlayerController : MonoBehaviour
     public bool falling = false;
     private bool collisionPostule = false;
     private float quickDropTime = 0.0f;
-    private float quickDropDelay = 1.0f;
+    private float quickDropDelay = 0.1f;
 
     //Dash vars
     public bool canDash = true;
