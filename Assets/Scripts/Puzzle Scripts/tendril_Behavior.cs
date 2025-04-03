21e791978b529929b162d19c2eee2348254ab9de
ginetteGonzales <ginette.gonzales67@gmail.com>
2025-04-03 14:29:35 -0400

    tendril swing sfx in

--- Assets/Scripts/Puzzle Scripts/tendril_Behavior.cs
+++ Assets/Scripts/Puzzle Scripts/tendril_Behavior.cs
@@ -19,6 +19,7 @@ public class tendril_Behavior : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             m_animator.CrossFadeInFixedTime("Strike", 0.2f, 0, 0.2f);
+            this.GetComponent<AudioSource>().PlayOneShot(this.GetComponent<AudioSource>().clip); //play swinging sfx
         }
     }
 
